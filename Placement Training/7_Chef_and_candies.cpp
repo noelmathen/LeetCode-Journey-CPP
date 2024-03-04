26f1f80d9269a831068e1b5aa2d81f6583362e9e
Noel Mathen <noelmathen03@gmail.com>
2024-03-04 05:48:24 +0530

    chef and candies

--- Placement Training/7_Chef_and_candies.cpp
+++ Placement Training/7_Chef_and_candies.cpp
@@ -10,10 +10,14 @@ int main() {
     vector<int> answer;
     for (int i = 0; i < T; i++) {
         cin >> N >> X;
+        if (N<X){
+            answer.push_back(0);
+            continue;
+        }
         if((N-X)%4==0)
             answer.push_back((N-X)/4);
         else
-            answer.push_back((N-X)/4+1);
+            answer.push_back(((N-X)/4)+1);
     }
     for (auto i : answer) {
         cout << i << endl;
