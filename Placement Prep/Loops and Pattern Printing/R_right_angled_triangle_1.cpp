0f89997e3b95d334589531f467f8db9002a1c722
Noel Mathen <noelmathen03@gmail.com>
2024-07-02 18:24:59 +0530

    R_right_angled_triangle_1

---
+++ Placement Prep/Loops and Pattern Printing/R_right_angled_triangle_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
