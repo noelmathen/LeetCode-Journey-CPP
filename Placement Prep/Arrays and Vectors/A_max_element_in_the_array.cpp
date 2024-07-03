f343bf8b8a5abe03a6e43cf096e3444d48e9908b
Noel Mathen <noelmathen03@gmail.com>
2024-07-03 09:40:24 +0530

    A_max_element_in_the_array

--- Placement Prep/Arrays and Vectors/A_max_element_in_the_array.cpp
+++ Placement Prep/Arrays and Vectors/A_max_element_in_the_array.cpp
@@ -3,8 +3,39 @@
 #include<algorithm>
 using namespace std;
 
+// int main(){
+//     int n, input;
+//     long long int max=-9999999999;
+//     vector<int> ar;
+//     cin>>n;
+
+//     for(int i=0; i<n;i++){
+//         cin>>input;
+//         ar.push_back(input);
+//         if(input>max)
+//             max=input;
+//     }
+//     cout<<max<<endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 int main(){
-        
+    int n, input;
+    vector<int> ar;
+    cin>>n;
+
+    for(int i=0; i<n;i++){
+        cin>>input;
+        ar.push_back(input);
+    }
+    cout<<*max_element(ar.begin(), ar.end())<<endl;
 
     return 0;
 }
+
