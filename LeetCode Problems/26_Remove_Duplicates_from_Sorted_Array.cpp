6b804ec7df74ce8e320b5ba1bf4f12f8c0d3ca0a
Noel Mathen <noelmathen03@gmail.com>
2024-07-08 14:02:50 +0530

    remove duplicate elements modified

--- LeetCode Problems/26_Remove_Duplicates_from_Sorted_Array.cpp
+++ LeetCode Problems/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,17 +1,17 @@
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-//         set<int> newnums;
-//         for(auto it:nums){
-//             newnums.insert(it);
-//         }
-//         nums.clear();
-//         for(auto it:newnums){
-//             nums.push_back(it);
-//         }
-//         return nums.size();
-//     }
-// };
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> newnums;
+        for(auto it:nums){
+            newnums.insert(it);
+        }
+        nums.clear();
+        for(auto it:newnums){
+            nums.push_back(it);
+        }
+        return nums.size();
+    }
+};
 
 class Solution {
 public:
@@ -27,3 +27,16 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=0;
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums.at(i)!=nums.at(i+1)){
+                nums[++j]=nums.at(i+1);
+            }
+        }
+        return j+1;
+    }
+};
