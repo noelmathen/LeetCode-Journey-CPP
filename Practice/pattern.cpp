897d43ac3126c488bae4ed433ecc86191ba751c9
Noel Mathen <noelmathen03@gmail.com>
2024-07-17 23:19:25 +0530

    new pattern

--- Practice/pattern.cpp
+++ Practice/pattern.cpp
@@ -46,37 +46,80 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter number of levels: ";
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         cout<<endl;
+//         if(i==0){
+//             for(int s=0; s<n-i-1; s++){
+//                 cout<<" ";
+//             }
+//             cout<<"*";
+//         }
+//         else if(i==n-1){
+//             for(int s=0; s<n-i-1; s++){
+//                 cout<<" ";
+//             }
+//             for(int j=0; j<(2*n-1); j++){
+//                 cout<<"*";
+//             }
+//         }
+//         else{
+//             for(int s=0; s<n-i-1; s++){
+//                 cout<<" ";
+//             }
+//             cout<<"*";
+//             for(int s=0; s<2*i-1; s++){
+//                 cout<<" ";
+//             }
+//             cout<<"*";
+//         }
+//     }
+// }
+
+
+
 #include<iostream>
 using namespace std;
+
 int main(){
-    int n;
-    cout<<"Enter number of levels: ";
-    cin>>n;
-    for(int i=0; i<n; i++){
-        cout<<endl;
-        if(i==0){
-            for(int s=0; s<n-i-1; s++){
-                cout<<" ";
-            }
+    int n=6;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
             cout<<"*";
         }
-        else if(i==n-1){
-            for(int s=0; s<n-i-1; s++){
-                cout<<" ";
-            }
-            for(int j=0; j<(2*n-1); j++){
-                cout<<"*";
-            }
+        for(int j=i; j<=n; j++){
+            cout<<" ";
         }
-        else{
-            for(int s=0; s<n-i-1; s++){
-                cout<<" ";
-            }
-            cout<<"*";
-            for(int s=0; s<2*i-1; s++){
-                cout<<" ";
-            }
+        for(int j=i; j<=n+1; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<i; j++){
             cout<<"*";
         }
+        cout<<"\n";
     }
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            cout<<"*";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<i; j++){
+            cout<<" ";
+        }
+        for(int j=i; j<=n; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
+
+
+
