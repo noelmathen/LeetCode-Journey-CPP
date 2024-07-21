96c1790889c2e212c81b637389e6395ded2ca077
Noel Mathen <noelmathen03@gmail.com>
2024-07-21 23:25:12 +0530

    242_Valid_Anagram

---
+++ LeetCode Problems/451_Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,21 @@
+//My solution
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> mapp;
+        string result="";
+        multimap<int, char, greater<int>> multimapp;
+        for(auto i: s){
+            mapp[i]++;
+        }
+        for(auto x : mapp){
+            multimapp.insert({x.second, x.first});
+        }
+        for(auto y : multimapp){
+            for(int i=1; i<=y.first; i++){
+                result+=y.second;
+            }
+        }
+        return result;
+    }
+};
