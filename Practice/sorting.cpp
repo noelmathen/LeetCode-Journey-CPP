15d018462c91863666997ef9fef7a72341593c7b
Noel Mathen <noelmathen03@gmail.com>
2024-08-08 10:40:25 +0530

    merge sort

--- Practice/sorting.cpp
+++ Practice/sorting.cpp
@@ -38,21 +38,56 @@ void insertion_sort(vector<int>& arr){
     }
 }
 
+void merge(vector<int>& arr, int low, int mid, int high){
+    vector<int> tempVector;
+    int l=low, r=mid+1;
+    while(l<=mid && r<=high){
+        if(arr[l]<=arr[r]){
+            tempVector.push_back(arr[l++]);
+        }
+        else{
+            tempVector.push_back(arr[r++]);
+        }
+    }
+    while(l<=mid){
+        tempVector.push_back(arr[l++]);
+    }
+    while(r<=high){
+        tempVector.push_back(arr[r++]);
+    }
+
+    for(int i=low; i<=high; i++){
+        arr[i]=tempVector[i-low];
+    }
+}
+
+void merge_sort(vector<int>& arr, int low, int high){
+    int n=arr.size();
+    if(low>=high)
+        return;
+
+    int mid = (low+high)/2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
 int main(){
     int n, min, x;
-    vector<int> arr;
-    cout<<"Enter the number of elements: ";
-    cin>>n;
-    for(int i=0; i<n; i++){
-        cin>>x;
-        arr.push_back(x);
-    }
+    vector<int> arr={8, 6, 1, 6, 7, 3, 9, 8, 2, 4 ,6 ,8 ,3 , 35};
+    // cout<<"Enter the number of elements: ";
+    // cin>>n;
+    // for(int i=0; i<n; i++){
+    //     cin>>x;
+    //     arr.push_back(x);
+    // }
 
     // selection_sort(arr);
     // bubble_sort(arr);
-    insertion_sort(arr);
+    // insertion_sort(arr);
 
-    
+    n=arr.size();
+    merge_sort(arr, 0, n-1);
 
     for(auto i:arr){
         cout<<i<<" ";;
---
+++ Practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+for(int i=low; i<=high; i++){
+        arr[i]=tempVector[i-low];
+        cout<<"hi1"<<endl;
+    }
