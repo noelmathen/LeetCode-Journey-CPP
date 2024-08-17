0aaa0eda3bb0632c9ef8426afe5e9131281eb568
Noel Mathen <noelmathen03@gmail.com>
2024-08-17 15:53:16 +0530

    Delete_head_of_DLL

---
+++ Practice/Basics of LinkedList/Delete_head_of_DLL.cpp
@@ -0,0 +1,41 @@
+/*
+Definition of doubly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode * deleteHead(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return NULL;
+        ListNode *temp = head;
+        head = head->next;
+        head->prev = NULL;
+        temp->next = NULL;
+        delete temp;
+        return head;
+    }
+};
