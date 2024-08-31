88aa17f96dfead49704535ccdf22026d9303e702
Noel Mathen <noelmathen03@gmail.com>
2024-08-31 12:38:29 +0530

    fibonacci_triangle

---
+++ GeekForGeeks_Other_Sites Problems.cpp/fibonacci_triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>  
+using namespace std;  
+int fibonacci(int n){
+    if(n==1 || n==2)
+        return 1;
+    return fibonacci(n-1) + fibonacci(n-2); 
+}
+int main()  
+{  
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++)
+            cout<<fibonacci(j)<<"   ";
+        cout<<endl;
+    }
+    return 0;  
+}  
