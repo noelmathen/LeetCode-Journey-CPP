d8d21dcd3ca365ec61c7572d6eaf7baedef37d28
Noel Mathen <noelmathen03@gmail.com>
2024-09-23 19:51:40 +0530

    pookalam techonam fest

--- Practice/pattern.cpp
+++ Practice/pattern.cpp
@@ -148,40 +148,71 @@
 
 
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int n=6;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<i; j++){
-            cout<<n-j+1;
-        }
-        for(int j=i; j<n; j++){
-            cout<<n-i+1;
-        }
-        for(int j=i; j<n+1; j++){
-            cout<<n-i+1;
-        }
-        for(int j=1; j<i; j++){
-            cout<<n-i+j+1;
-        }
-        cout<<"\n";
-    }
-    for(int i=2; i<=n; i++){
-        for(int j=i; j<n; j++){
-            cout<<n-j+i;
-        }
-        for(int j=1; j<i; j++){
-            cout<<i;
-        }
-        for(int j=1; j<i; j++){
-            cout<<i;
-        }
-        for(int j=i; j<=n; j++){
-            cout<<j;
-        }
-        cout<<"\n";
-    }
-    return 0;
-}
+// int main(){
+//     int n=6;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<i; j++){
+//             cout<<n-j+1;
+//         }
+//         for(int j=i; j<n; j++){
+//             cout<<n-i+1;
+//         }
+//         for(int j=i; j<n+1; j++){
+//             cout<<n-i+1;
+//         }
+//         for(int j=1; j<i; j++){
+//             cout<<n-i+j+1;
+//         }
+//         cout<<"\n";
+//     }
+//     for(int i=2; i<=n; i++){
+//         for(int j=i; j<n; j++){
+//             cout<<n-j+i;
+//         }
+//         for(int j=1; j<i; j++){
+//             cout<<i;
+//         }
+//         for(int j=1; j<i; j++){
+//             cout<<i;
+//         }
+//         for(int j=i; j<=n; j++){
+//             cout<<j;
+//         }
+//         cout<<"\n";
+//     }
+//     return 0;
+// }
+
+
+
+// //Pookalam
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n=9;
+//     for(int i=1; i<=n; i++)
+//         cout<<" * ";
+//     cout<<"\n";
+
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             if  ( i==1 || i==n || j==1 || j==n ||
+//                  ((i==2 || i==8) && (j==2 || j==3 || j==7 || j==8)) ||
+//                  ((i==3 || i==4 || i==5 || i==6 || i==7) && (j==2 || j==4 || j==5 || j==6 || j==8))
+//                 )
+//                 cout<<" * ";
+//             else
+//                 cout<<"   ";
+//         }
+//         cout<<"\n";
+//     }
+//     for(int i=1; i<=n; i++)
+//         cout<<" * ";
+//     cout<<"\n";
+
+//     return 0;
+// }
