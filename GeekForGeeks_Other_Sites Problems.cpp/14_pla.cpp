6ba49678c6cc513845aaeca5a8c3db8c0faf5469
Noel Mathen <noelmathen03@gmail.com>
2024-10-12 23:34:52 +0530

    palindrome quesiton but not complete

---
+++ GeekForGeeks_Other_Sites Problems.cpp/14_pla.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<algorithm>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string s){
+    int i=0, j=s.size()-1;
+    while(i<=j){
+        if(s[i++] != s[j--])
+            return false;
+    }
+    return true;
+}
+
+int calculateScore(string s) {
+    int n = s.length();
+    int score = 0;
+    for(int i=0; i<n-3; i++){
+        string newString="";
+        for(int j=i; j<n; j++){
+            newString += s[j];
+            if(j-i+1 == 4 && isPalindrome(newString))
+                score += 5;
+            else if(j-i+1 == 5 && isPalindrome(newString))
+                score += 10;
+        }
+    }
+    return score;
+}
+
+int main() {
+    // Test case 1
+    string input1 = "ABABAAAA";
+    int result1 = calculateScore(input1);
+    cout << "Test Case 1: Input: " << input1 << " | Expected Output: 15 | Your Output: " << result1 << endl;
+
+    // Test case 2
+    string input2 = "ABBAABBA";
+    int result2 = calculateScore(input2);
+    cout << "Test Case 2: Input: " << input2 << " | Expected Output: 5 | Your Output: " << result2 << endl;
+
+    // Test case 3
+    string input3 = "AAAAA";
+    int result3 = calculateScore(input3);
+    cout << "Test Case 3: Input: " << input3 << " | Expected Output: 10 | Your Output: " << result3 << endl;
+
+    // Test case 4
+    string input4 = "ABCDE";
+    int result4 = calculateScore(input4);
+    cout << "Test Case 4: Input: " << input4 << " | Expected Output: 0 | Your Output: " << result4 << endl;
+
+    // Test case 5
+    string input5 = "ABABABABA";
+    int result5 = calculateScore(input5);
+    cout << "Test Case 5: Input: " << input5 << " | Expected Output: 20 | Your Output: " << result5 << endl;
+
+    return 0;
+}
