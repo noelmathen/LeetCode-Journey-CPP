6f56eedcb16a93e77c9aaaf46e6db6102e30131c
Noel Mathen <noelmathen03@gmail.com>
2025-02-26 17:33:01 +0530

    17_Letter_Combinations_of_a_Phone_Number

---
+++ Striver/Recursion/Advanced/Letter_Combinations_of_a_Phone_Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        void letterComb(int i, string digits, vector<string>& combos, string str, vector<string>& result){
+            if(i==digits.size()){
+                result.push_back(str);
+                return;
+            }
+            int number = digits[i]-'0'; //to convert character to int
+            for(int j=0; j<combos[number].size(); j++){
+                letterComb(i+1, digits, combos, str+combos[number][j], result);
+            }
+        }
+    
+        vector<string> letterCombinations(string digits) {
+            vector<string> combos = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+            string str="";
+            vector<string> result;
+            letterComb(0, digits, combos, str, result);
+            if(digits.empty())
+                return {};
+            return result;
+        }
+};
