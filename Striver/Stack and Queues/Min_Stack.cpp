f2ed92f2824c9593e6e6a8394da59c0d23f57765
Noel Mathen <noelmathen03@gmail.com>
2025-07-16 21:31:18 +0530

    Min_Stack

---
+++ Striver/Stack and Queues/Min_Stack.cpp
@@ -0,0 +1,50 @@
+
+class MinStack {
+private:
+    int* stk;
+    int* minStk;
+    int i, j;
+
+public:
+    MinStack() {
+        i=-1;
+        j=-1;
+        stk = new int[10000];
+        minStk = new int[10000];
+    }
+
+    void push(int value) {
+        if(i==-1 || j==-1){
+            stk[++i] = value;
+            minStk[++j] = value;
+            return;
+        }
+        stk[++i] = value;
+        if(value < minStk[j])
+            minStk[++j] = value;
+        else{
+            ++j;
+            minStk[j] = minStk[j-1];
+        }
+        // cout<<minStk[j]<<endl;
+    }
+
+    void pop() {
+        if(i==-1)
+            return;
+        i--;
+        j--;
+    }
+
+    int top() {
+        if(i==-1)
+            return -1;
+        return stk[i];
+    }
+
+    int getMin() {
+        if(j==-1)
+            return -1;
+        return minStk[j];
+    }
+};
