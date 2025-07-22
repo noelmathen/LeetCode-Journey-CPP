39aba9883fb8b9defaa64a8324ea6aae05eb1968
Noel Mathen <noelmathen03@gmail.com>
2025-07-22 18:55:42 +0530

    104_Maximum_Depth_of_Binary_Tree

--- LeetCode Problems/104_Maximum_Depth_of_Binary_Tree.cpp
+++ LeetCode Problems/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -1,12 +1,12 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int data;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
-//  * };
-//  **/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
 
 // class Solution {
 // public:
