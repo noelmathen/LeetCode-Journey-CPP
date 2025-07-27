e0a6a3ccc3cec6532b337a49e1ceb77d64b2706e
Noel Mathen <noelmathen03@gmail.com>
2025-07-27 21:36:52 +0530

    15_Top_View_of_BT

---
+++ Striver/Binary Trees/15_Top_View_of_BT.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution{
+    public:
+    vector<int> topView(TreeNode *root){
+        vector<int> result;
+        queue<pair<TreeNode*, int>> q;
+        map<int, int> mapp;
+        int level=0;
+        q.push({root, level});
+
+        while(!q.empty()){
+            
+            int n = q.size();
+
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front().first;
+                level = q.front().second;
+
+                if(mapp.find(level)==mapp.end())
+                    mapp[level] = node->data;
+                
+                q.pop();
+                if(node->left)
+                    q.push({node->left, level-1});
+                if(node->right)
+                    q.push({node->right, level+1});
+            }
+        }
+        for(auto x : mapp){
+            result.push_back(x.second);
+        }
+        
+        return result;
+
+    }
+};
