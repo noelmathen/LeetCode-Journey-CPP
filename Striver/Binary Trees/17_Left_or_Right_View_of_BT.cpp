6293a6b132d2578201171a1c7fcc1b8318923d6e
Noel Mathen <noelmathen03@gmail.com>
2025-07-27 22:03:27 +0530

    17_Left_or_Right_View_of_BT

---
+++ Striver/Binary Trees/17_Left_or_Right_View_of_BT.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    void traverse(TreeNode* root, int level, map<int, int>& mapp){
+        if(!root)
+            return;
+        
+        mapp[level] = root->data;
+        traverse(root->left, level+1, mapp);
+        traverse(root->right, level+1, mapp);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        map<int, int> mapp;
+        vector<int> result;
+        traverse(root, 0, mapp);
+
+        for(auto x : mapp){
+            result.push_back(x.second);
+        }
+        return result;
+    }
+};
