f10d078002842f5a987c46ebda7d2a9b4059e506
Noel Mathen <noelmathen03@gmail.com>
2025-08-02 13:34:08 +0530

    106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal

---
+++ LeetCode Problems/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    unordered_map<int, int> mapp;
+    int postIndex;
+public:
+    TreeNode* makeTree(vector<int>& postOrder, int inStart, int inEnd){
+        if(inStart > inEnd)
+            return NULL;
+        
+        TreeNode* root = new TreeNode(postOrder[postIndex--]);
+        int inPos = mapp[root->val];
+
+        root->right = makeTree(postOrder, inPos+1, inEnd);
+        root->left = makeTree(postOrder, inStart, inPos-1);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = inorder.size();
+        for(int i=0; i<n; i++)
+            mapp[inorder[i]] = i;
+        
+        postIndex=n-1;
+        return makeTree(postorder, 0, n-1);
+    }
+};
