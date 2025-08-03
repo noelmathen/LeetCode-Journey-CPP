1d0037e0f81d1c5fa172496778e6f3be05804498
Noel Mathen <noelmathen03@gmail.com>
2025-08-03 12:04:03 +0530

    297_Serialize_and_Deserialize_Binary_Tree

---
+++ Striver/Binary Trees/27_Serialize_and_De-serialize_BT.cpp
@@ -0,0 +1,122 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    string addString(TreeNode* node){
+        if(!node)
+            return " N";
+        return " " + to_string(node->data);
+    }
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(root==NULL)
+            return "";
+        
+        string str;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        str.append(to_string(root->data));
+        
+        while(!q.empty()){
+            int n = q.size();
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front();
+                q.pop();
+
+                str.append(addString(node->left));
+                if(node->left)
+                    q.push(node->left);
+                    
+                str.append(addString(node->right));
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+        // cout<<str<<endl;
+        return str;
+    }
+
+    int getData(string& data, int& i){
+        string str;
+        if(data[i]=='N'){
+            i+=2;
+            return -1001;
+        }
+        
+        bool isNegative=false;
+        if(data[i]=='-'){
+            i++;
+            isNegative=true;
+        }
+        while(data[i]!=' '){
+            str.push_back(data[i]);
+            i++;
+        }
+        i++;
+
+        if(isNegative)
+            return stoi(str)*-1;
+
+        return stoi(str);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data=="")
+            return NULL;
+
+        int i=0;
+        TreeNode* root = new TreeNode(getData(data, i));
+        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int n = q.size();
+            for(int j=0; j<n; j++){
+                TreeNode* node = q.front();
+                q.pop();
+
+                int left = getData(data, i);
+                if(left==-1001){
+                    node->left=NULL;
+                }
+                else{
+                    node->left = new TreeNode(left);
+                    q.push(node->left);
+                }
+                    
+
+                int right = getData(data, i);
+                if(right==-1001)
+                    node->right=NULL;
+                else{
+                    node->right =  new TreeNode(right);
+                    q.push(node->right);
+                }
+            }
+        }
+
+        return root;
+    }
+};
+
+
+/*
+* Your Codec object will be instantiated and called as such:
+* Codec* ser = new Codec();
+* Codec* deser = new Codec();
+* string tree = ser->serialize(root);
+* TreeNode* ans = deser->deserialize(tree);
+* return ans;
+*/
