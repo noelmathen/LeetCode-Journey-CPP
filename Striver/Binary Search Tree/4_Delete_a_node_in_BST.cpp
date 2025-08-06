83c02f9e109f6e97b902f1ed95ede8170c366cc9
Noel Mathen <noelmathen03@gmail.com>
2025-08-07 00:35:06 +0530

    4_Delete_a_node_in_BST

---
+++ Striver/Binary Search Tree/4_Delete_a_node_in_BST.cpp
@@ -0,0 +1,178 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+
+//BETTER, STRIVER SOLUTION
+class Solution {
+public:
+    TreeNode* findLarge(TreeNode* root){
+        while(root->right)
+            root = root->right;
+        return root;
+    }
+
+    TreeNode* deleteHelper(TreeNode* root){
+        if(!root->left)
+            return root->right;
+        if(!root->right)
+            return root->left;
+        
+        TreeNode* largettLeftSubTreeNode = findLarge(root->left);
+        TreeNode* rightChild = root->right;
+        
+        largettLeftSubTreeNode->right = rightChild;
+        return root->left;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* originalRoot = root;
+        if(!root)
+            return NULL;
+        
+        if(root->data==key)
+            return deleteHelper(root);
+
+        while(root){
+            if(key<root->data){
+                if(root->left && root->left->data==key){
+                    root->left = deleteHelper(root->left);
+                    break;
+                }
+                else
+                    root = root->left;
+            }
+            else{
+                if(root->right && root->right->data==key){
+                    root->right = deleteHelper(root->right);
+                    break;
+                }
+                else
+                    root = root->right;
+            }
+        }
+
+        return originalRoot;
+    }
+};
+
+
+
+
+
+// // MY STUPID SOLUTION(But still the TC and SC is good)
+// class Solution {
+// public:
+//     TreeNode* deleteNode(TreeNode* root, int key) {
+//         TreeNode* originalRoot=root;
+//         TreeNode* prev=root;
+//         TreeNode* temp;
+//         //Finding if key exists or not
+//         while(root && root->data!=key){
+//             prev = root;
+//             if(key > root->data)
+//                 root = root->right;
+//             else
+//                 root = root->left;
+            
+//         }
+
+//         //if there is no key
+//         if(!root)
+//             return originalRoot;
+
+//         //Cases after key is found
+
+//         //Leaf Node
+//         if(!root->left && !root->right){
+//             if(key==originalRoot->data){
+//                 return NULL;
+//             }
+//             if(prev->right==root){
+//                 prev->right=NULL;
+//                 delete root;
+//             }
+//             if(prev->left==root){
+//                 prev->left=NULL;
+//                 delete root;
+//             }
+//         }
+
+//         else if(!root->left && root->right){
+//             if(key==originalRoot->data){
+//                 return originalRoot->right;
+//             }
+//             if(prev->right==root){
+//                 prev->right=root->right;
+//                 root->right=NULL;
+//                 delete root;
+//             }
+//             if(prev->left==root){
+//                 prev->left=root->right;
+//                 root->left=NULL;
+//                 delete root;
+//             }
+//         }
+
+//         else if(root->left && !root->right){
+//             if(key==originalRoot->data){
+//                 return originalRoot->left;
+//             }
+//             if(prev->right==root){
+//                 prev->right=root->left;
+//                 root->left=NULL;
+//                 delete root;
+//             }
+//             if(prev->left==root){
+//                 prev->left=root->left;
+//                 root->left=NULL;
+//                 delete root;
+//             }
+//         }
+        
+//         else if(root->left && root->right){
+//             TreeNode* temp=prev->right;
+            
+//             TreeNode* leftChild = root->left;
+//             TreeNode* rightChild = root->right;
+//             int ceil=INT_MAX;
+//             TreeNode* ceilNode = rightChild;
+//             while(rightChild!=NULL){
+//                 if(rightChild->data>=leftChild->data && rightChild->data<ceil){
+//                     ceil = rightChild->data;
+//                     ceilNode = rightChild;
+//                 }
+
+//                 if(leftChild->data > rightChild->data)
+//                     rightChild = rightChild->right;
+//                 else
+//                     rightChild = rightChild->left;
+//             }
+//             ceilNode->left = leftChild;
+            
+            
+
+//             if(prev->right==root){
+//                 prev->right=root->right;
+//                 root->left=NULL;
+//                 delete root;
+//             }
+//             if(prev->left==root){
+//                 prev->left=root->right;
+//                 root->left=NULL;
+//                 delete root;
+//             }
+//             if(key==originalRoot->data){
+//                 return temp;
+//             }
+//         }
+
+//         return originalRoot;
+//     }
+// };
