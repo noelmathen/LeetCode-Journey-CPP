4a23cd5fea3a7d8cbca9b7942930846a84008428
noelmathen <noelmathen03@gmail.com>
2025-08-10 12:41:25 +0530

    10_BST_iterator

---
+++ LeetCode Problems/173_Binary_Search_Tree_Iterator.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// STRIVER SOLUTION - O(1) time and O(h) Space
+class BSTIterator {
+private:
+    stack<TreeNode*> stk;
+public:
+    void pushLeftNodes(TreeNode* root){
+        while(root){
+            stk.push(root);
+            root = root->left;
+        }
+    }
+
+    BSTIterator(TreeNode* root) {
+        pushLeftNodes(root);
+    }
+    
+    int next() {
+        TreeNode* node = stk.top();
+        stk.pop();
+        if(node->right)
+            pushLeftNodes(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !stk.empty();
+    }
+};
+
+
+
+// // MY SOLUTION - O(1) time and O(n) Space
+// class BSTIterator {
+// private:
+//     vector<int> nodes;
+//     int pointer;
+// public:
+//     void traverse(TreeNode* root){
+//         if(!root)
+//             return;
+//         traverse(root->left);
+//         nodes.push_back(root->val);
+//         traverse(root->right);
+//     }
+
+//     BSTIterator(TreeNode* root) {
+//         traverse(root);
+//         pointer=-1;
+//     }
+    
+//     int next() {
+//         return nodes[++pointer];
+//     }
+    
+//     bool hasNext() {
+//         return pointer+1<nodes.size();
+//     }
+// };
+
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
